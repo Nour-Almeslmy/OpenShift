deb754d9fe55649c288f6a7858700ad77aae370e
Nour Almelsmy <91690116+Nour-Almeslmy@users.noreply.github.com>
2023-02-08 10:45:31 +0200

    Add project files.

---
+++ OpenShift_Area/Controllers/AreasController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenShift_Area.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AreasController : ControllerBase
+    {
+        [HttpGet]
+        public string GetPerimeter()
+        {
+            return "Areas perimeter";
+        }
+
+    }
+}
